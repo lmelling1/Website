9fae4e619cb4f9cdffc18251a69d086a5b7e9820
lmelling1 <lmelling@stud.ntnu.no>
2026-07-11 20:36:38 +0200

    pomodoro page started

---
+++ pomodoro.js
@@ -0,0 +1,45 @@
+const start = document.getElementById("start");
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+let timeLeft = 3;
+let interval;
+
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+
+    timer.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+const startTimer = () => {
+      if (interval) {
+            clearInterval(interval);
+      }
+
+      interval = setInterval(() => {
+            timeLeft--;
+            updateTimer();
+
+            if (timeLeft === 0) {
+                  clearInterval(interval);
+                  timeLeft = 1500;
+                  updateTimer();
+            }
+      }, 1000);
+};
+
+const pauseTimer = () => {
+      clearInterval(interval);
+}
+
+const resetTimer = () => {
+      clearInterval(interval);
+      timeLeft = 1500;
+      updateTimer();
+}
+
+start.addEventListener("click", startTimer);
+pause.addEventListener("click", pauseTimer);
+reset.addEventListener("click", resetTimer);
